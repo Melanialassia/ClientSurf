246d8face593edc9fd5be91c3b6bdbddcbecdc17
Agustin Prieto <aguspv99@gmail.com>
2024-01-11 20:00:02 -0300

    iniciosDeDetail

--- client/client/src/App.jsx
+++ client/client/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import {Routes, Route} from 'react-router-dom';
 
 import Home from './components/features/Home/roots/Home';
+import Details from './components/features/Details/roots/Details';
 
 const App = () => {
 
@@ -11,6 +12,7 @@ const App = () => {
 <div>
 <Routes>
   <Route path='/' element={<Home/>}/>
+  <Route path='/details/:id' element={<Details/>}/>
 </Routes>
 </div>
   )
