21ed43af9861396a6dca6f4dd4a105368aa0fe37
Agustin Prieto <aguspv99@gmail.com>
2024-01-11 20:05:55 -0300

    InicioDetails

--- client/client/src/App.jsx
+++ client/client/src/App.jsx
@@ -4,6 +4,7 @@ import {Routes, Route} from 'react-router-dom';
 
 import Home from './components/features/Home/roots/Home';
 import Details from './components/features/Details/roots/Details';
+import LoginForm from './components/features/LoginForm/roots/LoginForm';
 
 const App = () => {
 
@@ -13,6 +14,8 @@ const App = () => {
 <Routes>
   <Route path='/' element={<Home/>}/>
   <Route path='/details/:id' element={<Details/>}/>
+  <Route path='/account/create' element={<LoginForm/>}/>
+
 </Routes>
 </div>
   )
