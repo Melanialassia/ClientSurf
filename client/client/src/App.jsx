87cfe2af4aa6088cfad78fca1efba1a546d3087f
Agustin Prieto <aguspv99@gmail.com>
2024-01-12 18:14:25 -0300

    localStorage Y link de details con product page

--- client/client/src/App.jsx
+++ client/client/src/App.jsx
@@ -6,6 +6,7 @@ import Home from './components/features/Home/roots/Home';
 import LoginForm from './components/features/LoginForm/roots/LoginForm';
 import Details from './components/features/Details/roots/Details';
 import ProductPage from './components/features/ProductPage/roots/ProductPage';
+import Cart from './components/features/Cart/roots/Cart';
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
   <Route path='/account/create' element={<LoginForm/>}/>
   <Route path='/details/:id' element={<Details/>}/>
   <Route path='/producPage' element={<ProductPage/>}/>
+  <Route path='/cart' element={<Cart/>}/>
 </Routes>
 </div>
   )
