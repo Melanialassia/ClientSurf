d31f7487f5eb0eba1b3157cffc417da83b4f48cc
Agustin Prieto <aguspv99@gmail.com>
2024-01-14 17:05:27 -0300

    Details

--- client/client/src/components/features/Details/roots/Details.jsx
+++ client/client/src/components/features/Details/roots/Details.jsx
@@ -1,68 +1,182 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
 import { useParams, Link } from 'react-router-dom';
 import styles from './details.module.css';
 
-
 const Details = () => {
-    const { id } = useParams();
-    const [product, setProduct] = useState({});
-    const [cart, setCart] = useState([]);
-    console.log(id);
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-              const response = await axios(`http://localhost:3001/surf/detail/${id}`);
-              const { data } = response;
-              console.log(data);
+  const { id } = useParams();
+  const [product, setProduct] = useState({});
+  const [selectedSize, setSelectedSize] = useState('');
+  const [selectedColor, setSelectedColor] = useState('');
+  const imgRef = useRef(null); // Referencia para el elemento de imagen
 
-              if (data.name) {
-                setProduct(data);
-              }
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios(`http://localhost:3001/surf/product/${id}`);
+        const { data } = response;
+        setProduct(data.listProducts[0]);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, [id]);
+
+  useEffect(() => {
+    if (imgRef.current) {
+        const img = imgRef.current;
+
+        const handleMouseMove = (e) => {
+            const { left, top, width, height } = e.target.getBoundingClientRect();
+            const x = ((e.pageX - left) / width) * 100;
+            const y = ((e.pageY - top) / height) * 100;
+
+            // Adjust the scale factor for zooming
+            const scaleFactor = 1.4; // You can adjust this value as needed
+            img.style.transformOrigin = `${x}% ${y}%`;
+            img.style.transform = `scale(${scaleFactor})`;
+        };
+
+        const handleMouseOut = () => {
+            img.style.transformOrigin = 'center center';
+            img.style.transform = 'scale(1)';
+        };
+
+        img.addEventListener('mousemove', handleMouseMove);
+        img.addEventListener('mouseout', handleMouseOut);
+
+        return () => {
+            img.removeEventListener('mousemove', handleMouseMove);
+            img.removeEventListener('mouseout', handleMouseOut);
+        };
+    }
+}, [imgRef.current]);
+
+        console.log(product);
+
+        const filteredCharacteristics = Object.entries(product.characteristics || {})
+    .filter(([key, value]) => value !== true);
         
-              
-            } catch (error) {
-              
-            } 
-          };
-
-        fetchData(); }, [id]);
-
-        const addToCart = () => {
-          // Check if the product is already in the cart
-          const isInCart = cart.some(item => item.id === product.id);
-  
-          if (!isInCart) {
-              // If not, add it to the cart
-              const newCart = [...cart, product];
-              setCart(newCart);
-  
-              // Store the updated cart in localStorage
-              localStorage.setItem('cart', JSON.stringify(newCart));
-          } else {
-              console.warn('Product is already in the cart.');
-          }
+    const translateColor = (spanishColor) => {
+      const colorMap = {
+        gris: 'grey',
+        azul: 'blue',
+        verde:'green',
+        negro:'black'
+        // Agrega más traducciones según sea necesario
       };
-       console.log(localStorage.getItem('cart'));
+    
+      // Devuelve el color traducido o el color original si no hay traducción disponible
+      return colorMap[spanishColor.toLowerCase()] || spanishColor;
+    };
+
+    const handleSizeSelect = (size) => {
+      setSelectedSize(size);
+    };
+  
+    const handleColorSelect = (color) => {
+      setSelectedColor(color);
+    };
+  
+    const addToCart = () => {
+      // Aquí puedes enviar el producto al carrito con las tallas y colores seleccionados
+      console.log('Producto agregado al carrito:', {
+        ...product,
+        selectedSize,
+        selectedColor,
+      });
+      // Resto de la lógica para agregar al carrito...
+    };
+ 
+    console.log(selectedColor);
+    console.log(selectedSize);
+
+    
   return (
     <div>
         <div className={styles.detailsContainer}>
-            <div className={styles.imgContainer}>
-            <img src="https://http2.mlstatic.com/D_NQ_NP_756414-MLA48403444692_122021-O.webp" alt="ej" />
-            </div>
+        <div className={styles.imgContainer}>
+          <img
+            ref={imgRef}
+            src="https://http2.mlstatic.com/D_NQ_NP_756414-MLA48403444692_122021-O.webp"
+            alt="ej"
+            style={{ transform: 'scale(1.2)' }} // Asegúrate de que la imagen tenga un tamaño inicial más grande
+          />
+        </div>
             <div className={styles.infoContainer}>
            
             <div className={styles.subInfoContainer1}>
-              <h1>ESPERANDO RUTAS DE BACK DETAIL</h1>
-            <h2>Nombre de producto</h2>
-                <h3>Precio</h3>
+              
+            <h2>{product.name}</h2>
+                <h1>${product.priceProduct}</h1>
+                
+                <div className={styles.characteristics}>
+                <h3>Características:</h3>
+                <ul>
+                {filteredCharacteristics.map(([key, value]) => (
+  <li key={key}>
+    {key === 'Talla' ? (
+      // Si la característica es "Talla", renderiza botones
+      <>
+        <strong>{key}:</strong> {value.split(',').map((talla) => (
+          <button
+          key={talla}
+          style={{
+            marginRight: '5px',
+            border: talla === selectedSize ? '2px solid lightblue' : '2px solid transparent',
+          }}
+          onClick={() => handleSizeSelect(talla)}
+        >
+          {talla}
+        </button>
+        ))}
+      </>
+    ) : key === 'Color' ? (
+      // Si la característica es "Color", renderiza botones
+      <>
+        <strong>{key}:</strong> {value.split('/').map((color) => (
+          <button
+          key={color}
+          style={{
+            backgroundColor: translateColor(color),
+            marginRight: '5px',
+            width: '20px',
+            height: '20px',
+            border: `4px solid ${translateColor(color) === selectedColor ? 'lightblue' : 'transparent'}`,
+          }}
+          onClick={() => handleColorSelect(translateColor(color))}
+        >
+          {/* Puedes agregar aquí el texto o icono que desees */}
+        </button>
+        ))}
+      </>
+    ) : (
+      // Si no es "Talla" ni "Color", renderiza como texto
+      <>
+        <strong>{key}:</strong> {value}
+      </>
+    )}
+  </li>
+))}
+              </ul>
+              
             </div>
-            <h2>talle/medida</h2>
-            <p>Detalle de producto</p>
-                <button onClick={addToCart}>Añadir al carrito</button>
+          </div>
+        
+            <div className={styles.description}>
+              <h3>Descripcion:</h3>
+            <p>{product.description}</p>
+            </div>
+            <div className={styles.cartButton}>
+              <Link to='/cart'>
+                <button>Añadir al carrito</button>
+                </Link>
+            </div>
             </div>
         </div>
-            <div>
+            <div className={styles.politicas}>
                 <p>Politicas de Cambioo</p>
             </div>
 
--- client/client/src/components/features/ProductPage/components/Cards.jsx
+++ client/client/src/components/features/ProductPage/components/Cards.jsx
@@ -1,21 +1,26 @@
-import style from './Cards.module.css'
-import {Link} from "react-router-dom";
+
+import style from "./Cards.module.css";
+import { Link } from "react-router-dom";
+
+
+
 const Cards = ({ product }) => {
 
-    return (
-        <div>
-            <Link to={`/details/${product.id}`} >
-            <img
-                src={product.image}
-                alt="nombredelproducto"
-                className={style.productimage}
-            />
-            <h3>{product.idProduct}.{product.name}</h3>
-            <h4>${product.priceProduct}</h4>
-
-
-        </div>
-    )
+  return (
+    <div>
+      <Link to={`/details/${product.idProduct}`}>
+        <img
+          src={product.image}
+          alt="nombredelproducto"
+          className={style.productimage}
+        />
+        <h3>
+          {product.idProduct}.{product.name}
+        </h3>
+        <h4>${product.priceProduct}</h4>
+      </Link>
+    </div>
+  );
 };
 
 export default Cards;
