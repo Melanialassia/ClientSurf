7bb21d0d5c656e28dc75d928664b23851082e172
Mariana <mariana.crt@gmail.com>
2024-01-14 15:12:42 -0800

    footer

--- client/client/src/components/features/Header/components/NavBarButton.jsx
+++ client/client/src/components/features/Header/components/NavBarButton.jsx
@@ -3,17 +3,10 @@ import { Button } from "antd";
 
 const NavBarButton = ({ link, text }) => {
   return (
-    <Link to={link}>
-      {/* <Button
-        style={{
-          color: "#2D4056",
-          fontSize:"x-large",
-          marginTop: "-10px",
-        }}
-        type="text"
-      >
-        
-      </Button> */}
+    <Link to={link} style={{
+      color: "#30445c"
+    }}>
+
       {text}
     </Link>
   );
