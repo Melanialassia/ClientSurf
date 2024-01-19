5cad27600f431f95c7cd153587621756a2adf6e4
Agustin Prieto <aguspv99@gmail.com>
2024-01-19 14:35:40 -0300

    mejorasCarrito

--- client/client/src/components/features/Cart/roots/Cart.jsx
+++ client/client/src/components/features/Cart/roots/Cart.jsx
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import styles from './Cart.module.css';
 import { Link } from 'react-router-dom';
+import { DeleteOutlined } from '@ant-design/icons';
+
 
 const Cart = () => {
   const [cartData, setCartData] = useState(null);
- 
+  const [refreshCart, setRefreshCart] = useState(false);
   
   const fetchCartData = async () => {
     try {
@@ -18,18 +20,27 @@ const Cart = () => {
   
   useEffect(() => {
     fetchCartData();
-    
-  }, []); 
+    setRefreshCart(false);
+  }, [refreshCart]); 
   
   const cartListItems = cartData?.cartList || [];
   console.log(cartData);
   
   const handleRemoveProduct = async (productId) => {
     try {
-      await axios.delete(`http://localhost:3001/surf/cart/1`);
+      await axios.delete(`http://localhost:3001/surf/cart/1/${productId}`); 
       setRefreshCart(true);
     } catch (error) {
       console.error('Error al eliminar el producto del carrito:', error);
+    }
+  };
+
+  const handleRemoveAllProducts = async () => {
+    try {
+      await axios.delete(`http://localhost:3001/surf/cart/1`);
+      setRefreshCart(true);
+    } catch (error) {
+      console.error('Error al eliminar todos los productos del carrito:', error);
     }
   };
 
@@ -45,6 +56,7 @@ const Cart = () => {
         userId: "1",
       };
     });
+
     
     console.log(listCart);
     try {
@@ -59,47 +71,55 @@ const Cart = () => {
     }
   }
   
+  
   return (
-    <div className={styles.cartContainer}>
-      {cartListItems.length > 0 ? (
-        <div className={styles.productListContainer}>
-          <h2>Carrito de Compras</h2>
-          <ul className={styles.productList}>
-            {cartListItems.map((product) => (
-              <li key={product.idProduct} className={styles.productItem}>
+    
+      <div className={styles.cartContainer}>
+        {cartListItems.length > 0 ? (
+          <div className={styles.productListContainer}>
+            <h2>Carrito de Compras</h2>
+            <ul className={styles.productList}>
+              {cartListItems.map((product) => (
+                <li key={product.idProduct} className={styles.productItem}>
                   <div className={styles.productInfo}>
-                  <img src={product.image} alt={product.name} className={styles.productImage} />
-                <Link to={`/details/${product.idProduct}`} className={styles.productLink}>
-                    <p className={styles.productName}>{product.name}</p>
-                </Link>
+                    <img src={product.image} alt={product.name} className={styles.productImage} />
+                    <Link to={`/details/${product.idProduct}`} className={styles.productLink}>
+                      <p className={styles.productName}>{product.name}</p>
+                    </Link>
                     <p className={styles.productQuantity}>Cantidad: {product.amount}</p>
                     <p className={styles.productPrice}>Precio: ${product.priceProduct}</p>
                   </div>
-                <button
-                  className={styles.removeButton}
-                  onClick={() => handleRemoveProduct(product.idProduct)}
-                >
-                  Eliminar
-                </button>
-              </li>
-            ))}
-          </ul>
-        </div>
-      ) : (
-        <div>
+                  <button
+                    className={styles.customButton}
+                    onClick={() => handleRemoveProduct(product.idProduct)}
+                  >
+                    <DeleteOutlined />
+                  </button>
+                </li>
+              ))}
+            </ul>
+            <button  className={styles.customPrimaryButton} onClick={handleRemoveAllProducts}>
+              Vaciar Carrito
+            </button>
+          </div>
+        ) : (
+          <div>
             <h2>Carrito de Compras</h2>
-        <p>No tienes ningún artículo en tu carrito de compras.</p>
-        </div>
-      )}
-      {cartListItems.length > 0 && (
-        <div className={styles.totalPriceContainer}>
-          <p>Total de venta: <strong>${cartData?.costSale || 0}</strong></p>
-          <button className={styles.buyButton} onClick={handleFinish}>Finalizar Compra</button>
-        </div>
-      )}
-    </div>
+            <p>No tienes ningún artículo en tu carrito de compras.</p>
+          </div>
+        )}
+        {cartListItems.length > 0 && (
+          <div className={styles.totalPriceContainer}>
+            <p>Total de venta: <strong>${cartData?.costSale || 0}</strong></p>
+            <button className={styles.buyButton} onClick={handleFinish}>
+              FINALIZAR COMPRA
+            </button>
+          </div>
+        )}
+      </div>
+    
   );
-};
-
-export default Cart;
+  }
+  export default Cart;
+  
 
