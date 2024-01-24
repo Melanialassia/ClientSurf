6764c9e68fd2e41c9598aa9a2ada3f04f379bd3d
Mariana <mariana.crt@gmail.com>
2024-01-23 18:59:04 -0800

    estilos listos y funcionalidades

--- client/client/src/userComponents/features/UserDashboard/container/MyProfile.jsx
+++ client/client/src/userComponents/features/UserDashboard/container/MyProfile.jsx
@@ -2,7 +2,10 @@
 import React from "react";
 import { useSelector, useDispatch } from 'react-redux';
 import { useState, useEffect } from "react"; 
-import { Form, Link } from "react-router-dom";
+//COMPONENTS
+import EditUserData from "./EditUserData";
+//LIBRARYS
+import { Card } from 'antd';
 //CONSTANTS
 import { myAccount, personalInfo, editLink } from "../utils/constants";
 //REDUX
@@ -10,41 +13,53 @@ import { getIdUser } from "../../../../redux/actions/action";
 //STYLE-SHEETS
 import styles from "./MyProfile.module.css";
 
+const { Meta } = Card;
+
 const MyProfile = () => {
 
   const dispatch = useDispatch();
 
   const userData = useSelector((state) => state.userData);
  
-  const [userById, setUserById] = useState({
-    idUser: userData.idUser,
-    nameUser: userData.nameUser,
-    lastName: userData.lastName,
-    emailUser: userData.emailUser
-  })
-  
-  useEffect(() => {
-    // Actualiza userById cuando cambia userData
-    setUserById({
-      idUser: userData.idUser,
-      nameUser: userData.nameUser,
-      lastName: userData.lastName,
-      emailUser: userData.emailUser
-    });
-  }, [userData]);
+  const [isEditing, setIsEditing] = useState(false);
 
-  // useEffect(() => {
-  //   // Realiza la primera carga de datos al montar el componente
-  //   dispatch(getIdUser(userById.idUser));
-  // }, [dispatch]);
+  const handleEditClick = () => {
+    setIsEditing(true);
+  };
+
+  useEffect(() => {
+    if (userData.idUser) {
+      dispatch(getIdUser(userData.idUser));
+    }
+  }, [dispatch, userData.idUser]);
 
   return (
-    <div className={styles.container}>
-      <h3>{myAccount}</h3>
-      <h4>{personalInfo}</h4>
-      <Link to={"/edit-personalData"}>
-        <h4>{editLink}</h4>
-      </Link>
+
+    
+        <div className={styles.container}>
+    <Card
+    hoverable
+    style={{
+      width: 500,
+      height: 400,
+      margin: "10px",
+      padding: "0",
+      borderRadius: "2rem",
+    }}
+    className={styles.firstHeadline}
+
+  >
+      <div className={styles.profileInfo}>
+        <h3 className={styles.textt}>{myAccount}</h3>
+        <h4 className={styles.text}>{personalInfo}</h4>
+        <p className={styles.texto}>{userData.nameUser}</p>
+        <p className={styles.texto}>{userData.emailUser}</p>
+        <button onClick={handleEditClick} className={styles.editButton}>{editLink}</button>
+      </div>
+  </Card>
+      <div className={styles.editSection}>
+        {isEditing && <EditUserData onCancel={() => setIsEditing(false)} />}
+      </div>
     </div>
   );
 };
