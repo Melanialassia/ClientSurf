6c7bf6f5bf7fd4eff13da88dafb78e28b8e375fa
Mariana <mariana.crt@gmail.com>
2024-01-23 18:56:11 -0800

    estilos listo

--- client/client/src/components/features/Header/components/ProfileMenu.jsx
+++ client/client/src/components/features/Header/components/ProfileMenu.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 import { useDispatch } from 'react-redux';
 import { useNavigate } from "react-router-dom";
-//ACTIONS
+//REDUX
 import { logOut } from "../../../../redux/actions/action";
 //LIBRARY
 import { Menu } from "antd";
