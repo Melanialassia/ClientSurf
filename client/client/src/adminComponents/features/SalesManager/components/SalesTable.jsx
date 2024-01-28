cd79f93f7a56ccf0ff5c5209b55714bfe701a28d
Agustin Prieto <aguspv99@gmail.com>
2024-01-28 17:42:41 -0300

    DetailsVentaDash

--- client/client/src/adminComponents/features/SalesManager/components/SalesTable.jsx
+++ client/client/src/adminComponents/features/SalesManager/components/SalesTable.jsx
@@ -1,73 +1,121 @@
-import React, { useEffect, useState } from "react";
-import { Table, Tag, Space } from "antd";
-import axios from "axios";
+  import React, { useEffect, useState } from "react";
+  import { Table, Tag, Space, Modal } from "antd";
+  import axios from "axios";
+  import styles from "./SalesTable.module.css";
 
-const SalesTable = () => {
-  const [dataSales, setDataSales] = useState([]);
-  
-  useEffect(() => {
-    const fetchData = async () => {
+  const SalesTable = () => {
+    const [dataSales, setDataSales] = useState([]);
+    const [isModalVisible, setIsModalVisible] = useState(false);
+    const [saleDetails, setSaleDetails] = useState(null);
+    console.log(saleDetails);
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const response = await axios.get(
+            "https://surf-4i7c.onrender.com/surf/sale"
+          );
+          setDataSales(response.data.data);
+        } catch (error) {
+          console.error("Error fetching sales data:", error);
+        }
+      };
+
+      fetchData();
+    }, []);
+
+    const deleteSale = async (id) => {
       try {
-        const response = await axios.get(
-          "https://surf-4i7c.onrender.com/surf/sale"
-        );
-        setDataSales(response.data.data);
+        await axios.delete(`https://surf-4i7c.onrender.com/surf/sale/${id}`);
+        setDataSales(dataSales.filter((sale) => sale.idSale !== id));
       } catch (error) {
-        console.error("Error fetching sales data:", error);
+        console.error(`Error deleting sale ${id}:`, error);
       }
     };
 
-    fetchData();
-  }, []);
+    const showSaleDetails = async (idSale) => {
+      try {
+        const response = await axios.get(`https://surf-4i7c.onrender.com/surf/detail/${idSale}`);
+        const details = response.data;
+        setSaleDetails(details);
+        setIsModalVisible(true);
+      } catch (error) {
+        console.error(`Error getting sale details ${idSale}:`, error);
+      }
+    };
 
-  const deleteSale = async (id) => {
-    try {
-      await axios.delete(`https://surf-4i7c.onrender.com/surf/sale/${id}`);
-      setDataSales(dataSales.filter((sale) => sale.idSale !== id));
-    } catch (error) {
-      console.error(`Error deleting sale ${id}:`, error);
-    }
+    const handleOk = () => {
+      setIsModalVisible(false);
+    };
+  
+    
+
+    const columns = [
+      {
+        title: "ID",
+        dataIndex: "idSale",
+        key: "idSale",
+      },
+      {
+        title: "Nombre de Usuario",
+        dataIndex: "nameUser",
+        key: "nameUser",
+      },
+      {
+        title: "Correo Electronico",
+        dataIndex: "emailUser",
+        key: "emailUser",
+      },
+      {
+        title: "Fecha de compra",
+        dataIndex: "date",
+        key: "date",
+      },
+      {
+        title: "Costo Total",
+        dataIndex: "costSale",
+        key: "costSale",
+      },
+      {
+        
+        render: (_, record) => (
+          <Space size="middle">
+            
+            <a onClick={() => deleteSale(record.idSale)}>Eliminar</a>
+            <a onClick={() => showSaleDetails(record.idSale)}>Ver Detalles</a>
+          </Space>
+        ),
+      },
+    ];
+
+    return (
+      <>
+        <Table columns={columns} dataSource={dataSales} />
+        <Modal title="Detalles de Venta" visible={isModalVisible} onOk={handleOk} onCancel={handleOk}>
+  {saleDetails && saleDetails.data.map((product, index) => (
+    <div key={index} className={styles.productContainer}>
+      <img className={styles.detailImg} src={product.image} alt="Product Image" />
+      <div className={styles.productInfoTotal}>
+      <div>
+        <p><strong>Nombre: </strong>{product.name}</p>
+        <p><strong>Cantidad: </strong>{product.amount}</p>
+        <p><strong>Precio: </strong>${product.priceTotal}</p>
+        
+      </div>
+      <div>
+        <p><strong>Color: </strong>{product.nameColor}</p>
+        <p><strong>Talle: </strong>{product.nameSize}</p>
+        <p><strong>Marca: </strong>{product.brandName}</p>
+        </div>
+        </div>
+    </div>
+  ))}
+</Modal> 
+      </>
+    );
   };
 
-  const columns = [
-    {
-      title: "ID",
-      dataIndex: "idSale",
-      key: "idSale",
-    },
-    {
-      title: "User Name",
-      dataIndex: "nameUser",
-      key: "nameUser",
-    },
-    {
-      title: "Email",
-      dataIndex: "emailUser",
-      key: "emailUser",
-    },
-    {
-      title: "Date",
-      dataIndex: "date",
-      key: "date",
-    },
-    {
-      title: "Cost",
-      dataIndex: "costSale",
-      key: "costSale",
-    },
-    {
-      
-      render: (_, record) => (
-        <Space size="middle">
-          
-          <a onClick={() => deleteSale(record.idSale)}>Delete</a>
-        </Space>
-      ),
-    },
-  ];
+    
+  
 
-  return <Table columns={columns} dataSource={dataSales} />;
-};
-
-export default SalesTable;
+  export default SalesTable;
 
