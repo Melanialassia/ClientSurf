34026fcc47067c5dff908a19f7f94291203f37ec
kevsalcedo <kevsalcedom@gmail.com>
2024-01-30 00:38:16 -0500

    Actualizacion de review Page y agregar rate al detail

--- client/client/src/components/features/FavoritesPage/components/FavoriteCard/FavoriteCard.jsx
+++ client/client/src/components/features/FavoritesPage/components/FavoriteCard/FavoriteCard.jsx
@@ -1,16 +1,29 @@
 import React from "react";
 import { useDispatch } from "react-redux";
-import { deleteFavorite } from "../../../../../redux/actions/action";
+import { deleteFavorite, addToCart } from "../../../../../redux/actions/action";
 import { DeleteOutlined, ShoppingCartOutlined } from "@ant-design/icons";
-import { Button } from "antd";
+import { Button, message } from "antd";
 import styles from "./FavoriteCard.module.css";
 import { Link } from "react-router-dom";
 
 const FavoriteCard = ({ product, user }) => {
   const dispatch = useDispatch();
+  const [messageApi, contextHolder] = message.useMessage();
 
   const handleDelete = () => {
     dispatch(deleteFavorite(user.idUser, product.idProduct));
+    messageApi.open({
+      type: "success",
+      content: "Producto eliminado de favoritos!",
+    });
+  };
+
+  const handleAddToCart = () => {
+    dispatch(addToCart(product.idProduct, user.idUser, 1));
+    messageApi.open({
+      type: "success",
+      content: "Producto agregado al carrito!",
+    });
   };
 
   return (
@@ -26,7 +39,7 @@ const FavoriteCard = ({ product, user }) => {
       </div>
 
       <div className={styles.buttons}>
-        <Button type="primary" icon={<ShoppingCartOutlined />}>
+        <Button onClick={handleAddToCart} icon={<ShoppingCartOutlined />}>
           Agregar al carrito
         </Button>
 
--- client/client/src/userComponents/features/UserDashboard/roots/ReviewPage.jsx
+++ client/client/src/userComponents/features/UserDashboard/roots/ReviewPage.jsx
@@ -1,14 +1,47 @@
-import React from 'react'
-import ReviewCard from '../container/ReviewCard'
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import ReviewCard from "../container/ReviewCard";
+import styles from "./ReviewPage.module.css";
+//import { getSaleDetails } from "../../../../redux/actions/action";
+import axios from "axios";
 
-const ReviewPage = () => {
+const ReviewPage = ({ idSale, idUser }) => {
+  const dispatch = useDispatch();
+  const [userSale, setUserSale] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        await handlerGetSaleDetails(35); //TODO: cambiar por idSale
+      } catch (error) {
+        throw Error("No se pudo traer los objetos de la compra: ", error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const handlerGetSaleDetails = async (idSale) => {
+    const { data } = await axios.get(
+      `https://surf-4i7c.onrender.com/surf/detail/${idSale}`
+    );
+    const response = data.data;
+    setUserSale(response);
+  };
 
   return (
-    <div>
-      <p>Review Page</p>
-      <ReviewCard/>
+    < >
+      <p className={styles.title}>Califica tu compra</p>
+      <div className={styles.wrapper}>
+      {userSale.map((item) => (
+        <ReviewCard
+          key={item.idProduct}
+          idProduct={item.idProduct}
+          idUser={84} /* TODO: cambiar por idUser */
+        />
+      ))}
       </div>
-  )
-}
+    </>
+  );
+};
 
-export default ReviewPage
+export default ReviewPage;
